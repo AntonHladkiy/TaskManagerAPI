519682c1f51900be946d6d73bc5556a97d860da5
Anton <gladkyy@ukr.net>
2021-02-15 11:05:46 +0200

    added validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :priority, numericality: { greater_than: 0 }
+  validate :due_date_cannot_be_in_past
+
+  def due_date_cannot_be_in_past
+    if dueDate.present? && dueDate < Date.today
+      errors.add(:dueDate, "can't be in the past")
+    end
+  end
 end
