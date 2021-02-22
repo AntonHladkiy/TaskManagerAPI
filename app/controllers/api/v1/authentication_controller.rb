aeb55d5388ac21bfce9a948134af393cdc2122fa
Anton Hladkyi <gladkyy@ukr.net>
2021-02-22 18:41:31 +0200

    confirmation

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,11 +2,12 @@ class Api::V1::AuthenticationController < ApiController
   skip_before_action :authenticate_user!, only: [:create]
   def create
     user = User.find_by(email: params[:email])
-    puts user
-    if user&.valid_password?(params[:password])
-      render json: { token: JsonWebToken.encode(sub: user.id) }
-    else
-      render json: { errors: 'invalid' }
+    if !user.confirmed_at.nil?
+      if user&.valid_password?(params[:password])
+        render json: { token: JsonWebToken.encode(sub: user.id) }
+      else
+        render json: { errors: 'invalid' }
+      end
     end
   end
   def fetch
