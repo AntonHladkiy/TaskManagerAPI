c27122b24193155059a30c09da4dab7832174868
Anton <gladkyy@ukr.net>
2021-03-02 13:15:18 +0200

    user controller tests working

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,8 +2,6 @@ class Api::V1::AuthenticationController < ApiController
   skip_before_action :authenticate_user!, only: [:create]
   def create
     user = User.find_by(email: params[:email])
-    p params
-    p user
      if !user.confirmed_at.nil?
       if user&.valid_password?(params[:password])
         render json: { token: JsonWebToken.encode(sub: user.id) }
--- spec/controllers/login_spec.rb
+++ spec/controllers/login_spec.rb
@@ -18,7 +18,6 @@ RSpec.describe 'POST /api/v1/auth', type: :request do
   context 'when params are correct' do
     before do
       if(User.find_by(email: params[:email]).nil?)
-        p "check"
         Fabricate(:user)
       end
       post url, params: params
@@ -35,7 +34,6 @@ RSpec.describe 'POST /api/v1/auth', type: :request do
   context 'when login params are incorrect' do
     before do
       if(User.find_by(email: params[:email]).nil?)
-        p "check"
         Fabricate(:user)
       end
       post url, params: wrong_params
