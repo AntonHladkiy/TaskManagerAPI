caf68aa1e0cb82df14ca97efb22d25241973e3f5
Anton <gladkyy@ukr.net>
2021-03-03 12:06:21 +0200

    confirmation fixes

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,13 @@
 class ConfirmationsController < Devise::ConfirmationsController
-  def after_confirmation_path_for(resource_name, resource)
-    "https://toptaskmanager.herokuapp.com/success"
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to "https://toptaskmanager.herokuapp.com/success"}
+    else
+      redirect_to "https://toptaskmanager.herokuapp.com/"
+    end
   end
 end
